e8345b1a14346a3346655299a24d5cea4a8b11df
Tania <tanner.shirkey@gmail.com>
2024-04-29 18:29:23 -0400

    Implemented Appending to Table
    A user may now insert their own entries into the database.

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,6 +14,6 @@ VALUES ("HR Lead", 12.25, 004),
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES	("Doug", "Frankerson", 004, NULL),
 		("Jaimie", "Rodrick", 001, 001),
-		("Abbie", NULL, 005, 001),
+		("Abbie", "Frenelli", 005, 001),
 		("Dickerson", "Douch", 003, NULL),
 		("Carla", "Simplant", 002, 004)
--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
 });
 
+const capitalizeFirst = (word) => {
+  const wordList = word.split(" ");
+  const wordEnd = wordList.map((currentWord) => currentWord.slice(1).toLowerCase());
+  const firstLetter = wordList.map((currentWord) => currentWord.charAt(0).toUpperCase());
+  for (let i = 0; i < wordList.length; i++) {
+    wordList[i] = firstLetter[i] + wordEnd[i];
+  }
+  return wordList.join(" ");
+};
+
 const userChoices = [
   "View all departments",
   "View all roles",
@@ -22,7 +32,7 @@ const userChoices = [
 const selectTable = (table) => {
   connection.query(`SELECT * FROM ${table}`, (error, results) => {
     if (error) {
-      console.alert(error);
+      console.info(error);
     } else {
       // Try to remove the index of this table later if there's time
       console.table(results);
@@ -32,49 +42,106 @@ const selectTable = (table) => {
 };
 
 const addRow = (table) => {
-  switch (table) {
-    case "department":
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "What is the department you'd like to add?",
-            name: "title",
+  if (table === "department") {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the department you'd like to add?",
+          name: "title",
+        },
+      ])
+      .then((input) => {
+        connection.query(
+          `INSERT INTO department (name) VALUES ("${capitalizeFirst(input.title)}");`
+        );
+        selectTable(table);
+      });
+  } else if (table === "role") {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What role are you adding?",
+          name: "title",
+        },
+        {
+          type: "input",
+          message: "What is this role's average salary?",
+          name: "salary",
+        },
+        {
+          type: "input",
+          message: "Which department does this role belong to?",
+          name: "department",
+        },
+      ])
+      .then((input) => {
+        connection.query(`
+  					INSERT INTO role (title, salary, department_id)
+  					VALUES ("${capitalizeFirst(input.title)}", ${Number(input.salary)},
+  					(SELECT id FROM department 
+						WHERE name = "${capitalizeFirst(input.department)}"));`);
+        selectTable(table);
+      });
+  } else if (table === "employee") {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the employee's first name?",
+          name: "firstName",
+        },
+        {
+          type: "input",
+          message: "What is the employee's last name?",
+          name: "lastName",
+        },
+        {
+          type: "input",
+          message: "What is the employee's role?",
+          name: "role",
+        },
+        {
+          type: "confirm",
+          message: "Is the employee a manager?",
+          name: "isManager",
+        },
+        {
+          type: "input",
+          message: "Who is this employee's manager?",
+          name: "manager",
+          when(answers) {
+            return !answers.isManager;
           },
-        ])
-        .then((input) => {
-          connection.query(
-            `INSERT INTO department (name) VALUES ("${input.title}");`
-          );
-        });
-      break;
-    case "role":
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "What role are you adding?",
-            name: "title",
-          },
-          {
-            type: "input",
-            message: "What is this role's average salary?",
-            name: "salary",
-          },
-          {
-            type: "input",
-            message:
-              "Which department does this role belong to? Please input an ID number for that department.",
-            name: "department",
-          },
-        ])
-        .then((input) => {
+        },
+      ])
+      .then((input) => {
+        if (!input.isManager) {
           connection.query(`
-						INSERT INTO role (title, salary, department_id)
-						VALUES ("${input.title}", ${input.salary}, ${input.department});`);
-        });
+  					INSERT INTO employee (first_name, last_name, role_id, manager_id)
+  					VALUES 
+						('${capitalizeFirst(input.firstName)}', 
+							'${capitalizeFirst(input.lastName)}',
+  						(SELECT id FROM role WHERE title='${capitalizeFirst(input.role)}'),
+  						(SELECT id FROM employee WHERE first_name='${capitalizeFirst(input.manager)}'
+						);`);
+          selectTable(table);
+        } else {
+          connection.query(`
+  					INSERT INTO employee (first_name, last_name, role_id, manager_id)
+  					VALUES 
+						('${capitalizeFirst(input.firstName)}', 
+							'${capitalizeFirst(input.lastName)}',
+  						(SELECT id FROM role WHERE title='${capitalizeFirst(input.role)}'), 
+							NULL
+						);`);
+          selectTable(table);
+        }
+      });
+  } else {
+    console.log("Error: Could not parse input");
   }
-  selectTable(table);
 };
 
 const userPrompts = () => {
@@ -100,6 +167,12 @@ const userPrompts = () => {
           break;
         case userChoices[3]:
           addRow("department");
+          break;
+        case userChoices[4]:
+          addRow("role");
+          break;
+        case userChoices[5]:
+          addRow("employee");
           break;
         case userChoices[6]:
           process.exit();
